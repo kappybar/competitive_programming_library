9a6300807eda367e20149dbcdbf5bdf7103582de
kappybar <newton7233@outlook.jp>
2020-11-30 13:37:20 +0900

    change Dijkstra

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,58 +1,65 @@
 #include <bits/stdc++.h>
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define chmin(x,y) x = min((x),(y));
+#define chmax(x,y) x = max((x),(y));
 using namespace std;
 using ll = long long ;
 using P = pair<int,int> ;
 using pll = pair<long long,long long>;
-constexpr int INF = 1e9;
-constexpr long long LINF = 1e17;
-constexpr int MOD = 1000000007;
+const int INF = 1e9;
+const long long LINF = 1e17;
+const int MOD = 1000000007;
+//const int MOD = 998244353;
+const double PI = 3.14159265358979323846;
 
-int v = 100005;
-int e;
-vector<vector<pll>> graph(v,vector<pll>());
 
-vector<ll>  Dijkstra(int start){
-    vector<bool> seen(v,false);
-    vector<ll> shortest(v,LINF);
-    priority_queue<pll,vector<pll>,greater<pll>> pq;
-    pq.push(pll(0,start));
-
-    while(!pq.empty()){
-        ll dist = pq.top().first;
-        int node = pq.top().second;
-        pq.pop();
-        if(seen[node]) continue;
-
-        shortest[node] = dist;
-        seen[node] = true;
-
-        for(pll next:graph[node]){
-            int next_node = next.second;
-            ll next_cost = next.first;
-            if(seen[next_node]) continue;
-            pq.push(pll(next_cost + dist,next_node));
-        }
+//add_edge(a,b,d)でa->bにdの長さの辺を張る
+//Dijkstra(s)でsからの最短経路を求める。
+template<typename T>
+struct Dijkstra_graph{
+    using pp = pair<T,int>;
+    int n;
+    vector<vector<pp>> Graph;
+    Dijkstra_graph(int n):n(n){
+        Graph.resize(n);
     }
-
-    return shortest;
-}
+    void add_edge(int a,int b,T weight){
+        Graph[a].push_back(pp{weight,b});
+    }
+    vector<T> Dijkstra(int start){
+        vector<T> dist(n,LINF);
+        priority_queue<pp,vector<pp>,greater<pp>> pq;
+        pq.push(pp{0,start});
+        dist[start] = 0;
+        while(!pq.empty()){
+            int v = pq.top().second;
+            T d = pq.top().first;
+            pq.pop();
+            if(dist[v]<d) continue;
+            for(pp nxt:Graph[v]){
+                if(dist[nxt.second]<=dist[v]+nxt.first) continue;
+                dist[nxt.second] = dist[v] + nxt.first;
+                pq.push(pp{dist[nxt.second],nxt.second});
+            }
+        }
+        return dist;
+    }
+};
 
 int main(){
-    int start;
-    cin >>  v >> e >> start;
+    int v,e,r;
+    cin >> v >> e >> r;
+    Dijkstra_graph<ll> G(v);
     rep(i,e){
-        int s,t,d;
+        int s,t;
+        ll d;
         cin >> s >> t >> d;
-        graph[s].push_back(pll(d,t));
+        G.add_edge(s,t,d);
     }
-
-    vector<ll> ans = Dijkstra(start);
-
+    vector<ll> d = G.Dijkstra(r);
     rep(i,v){
-        if(ans[i]==LINF) cout << "INF" << endl;
-        else cout << ans[i] << endl;
+        if(d[i] >= LINF) cout << "INF" << endl;
+        else cout << d[i] << endl;
     }
-
     return 0;
 }

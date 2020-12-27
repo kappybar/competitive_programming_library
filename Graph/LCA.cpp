592a25c1761624af5e9f9c880f28aafba15c6a02
kappybar <newton7233@outlook.jp>
2020-12-27 17:35:56 +0900

    LCA

--- Graph/LCA.cpp
+++ Graph/LCA.cpp
@@ -14,32 +14,36 @@ const double PI = 3.14159265358979323846;
 
 
 struct LCA{
+    using p = pair<int,long long>;
     int n;//頂点の数
     int k;//(1<<k)<n
-    vector<vector<int>> tree;
+    vector<vector<p>> tree;
     vector<vector<int>> parents;//parents[i][j]:=jの2^iの祖先
     vector<int> depth;//rootからの深さ
+    vector<long long> weight;//rootからの距離
 
     LCA(int n):n(n){
         tree.resize(n);
         depth.resize(n);
+        weight.resize(n);
         k = 1;
         while((1<<k)<n) ++k;
         parents.assign(k,vector<int>(n,-1));
     }
 
-    //aからbの無向辺を張る
-    void add_edge(int a,int b){
-        tree[a].push_back(b);
-        tree[b].push_back(a);
+    //aからbの無向辺を張る(距離d)
+    void add_edge(int a,int b,long long d=1){
+        tree[a].emplace_back(b,d);
+        tree[b].emplace_back(a,d);
     }
 
-    void init_dfs(int i,int before=-1,int d=0){
+    void init_dfs(int i,int before=-1,int d=0,long long w=0){
         parents[0][i] = before;
         depth[i] = d;
-        for(int next:tree[i]){
-            if(next == before) continue;
-            init_dfs(next,i,d+1);
+        weight[i] = w;
+        for(p next:tree[i]){
+            if(next.first == before) continue;
+            init_dfs(next.first, i, d+1, w+next.second);
         }
         return;
     }
@@ -74,6 +78,10 @@ struct LCA{
             }
         }
         return parents[0][a];
+    }
+    //aとbの距離
+    long long dist(int a,int b){
+        return weight[a] + weight[b] - 2*weight[lca(a,b)];
     }
 };
 

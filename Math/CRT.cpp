16c339cc1b9f140b16ca79fecd432d8acd70a7b1
kappybar <newton7233@outlook.jp>
2021-02-28 18:12:21 +0900

    CRT.cpp

--- Math/CRT.cpp
+++ Math/CRT.cpp
@@ -1,73 +1,99 @@
 #include <bits/stdc++.h>
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define chmin(x,y) x = min((x),(y));
+#define chmax(x,y) x = max((x),(y));
 using namespace std;
 using ll = long long ;
 using P = pair<int,int> ;
 using pll = pair<long long,long long>;
-constexpr int INF = 1e9;
-constexpr long long LINF = 1e17;
-constexpr int MOD = 1000000007;
-constexpr double PI = 3.14159265358979323846;
-
-inline long long mod(long long x,long long m){
-    x %= m;
-    if(x < 0) x += m;
-    return x;
-}
+const int INF = 1e9;
+const long long LINF = 9e18;
+const int MOD = 1000000007;
+//const int MOD = 998244353;
+const double PI = 3.14159265358979323846;
 
 long long gcd(long long i,long long j){
     if(j == 0) return i;
     return gcd(j,i%j);
 }
 
-//a*x + b*y =gcd(a,b)を満たすx、yをもとめる
-pair<long long,long long> extgcd(long long a,long long b){
-    if(b == 0) return {1,0};
-    pair<long long,long long> res = extgcd(b,a%b);
-    long long x = res.first;
-    long long y = res.second;
-    return {y,x - (a/b) * y};
+long long lcm(long long i,long long j){
+    return i / gcd(i,j) * j;
 }
 
-pair<long long,long long> CRT(long long b1,long long m1,long long b2,long long m2){
-    pair<long long,long long> pq = extgcd(m1,m2);
-    long long p = pq.first;
-    long long q = pq.second;
-    long long d = gcd(m1,m2);
-    if((b2 - b1) % d != 0) return make_pair(0,-1);
-    long long m =  m1 * (m2 / d);          //lcm(m1,m2)
-    long long tmp = ((b2 - b1) / d * p) % (m2/d);
-    long long r = mod(b1 + m1 * tmp, m);
-    return make_pair(r,m);
+long long extgcd(long long a,long long b,long long &x,long long &y){
+    if(b == 0){// gcd(a,b) * 1 + 0 * 0 = gcd(a,b);
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long x2,y2;
+    long long g = extgcd(b,a%b,x2,y2);
+    x = y2;
+    y = x2 - (a/b) * y2;
+    return g;
+}
+
+pair<long long,long long> crt(const vector<long long> &r,const vector<long long> &m){
+    assert(r.size() == m.size());
+    int n = (int)r.size();
+
+    long long r0 = 0,m0 = 1;// x = 0 (mod 1)
+    for(int i=0;i<n;i++){
+        assert(1 <= m[i]);
+        long long r1 = r[i];
+        long long m1 = m[i];
+
+        r1 %= m1;
+        if(r1 < 0) r1 += m1;
+
+        if(m0 < m1){
+            swap(r0,r1);
+            swap(m0,m1);
+        }
+
+        if(m0 % m1 == 0){
+            if(r0 % m1 != r1) return {0,0};//解なし
+            continue;
+        }
+
+        long long g,im,tmp;
+        g = extgcd(m0,m1,im,tmp);
+        if((r1 - r0) % g != 0)  return {0,0};//解なし
+
+        long long u1 = m1 / g;
+        long long x = ((((r1 - r0) / g) % u1) * im) % u1;
+
+        r0 += x * m0;
+        m0 *= u1;
+        if(r0 < 0) r0 += m0;
+    }
+    return {r0,m0};
+}
+
+void solve(){
+    ll n,s,k;
+    cin >> n >> s >> k;
+    ll g = gcd(k,n);
+    if(s % g != 0) cout << -1 << endl;
+    else{
+        k /= g;
+        s /= g;
+        n /= g;
+        ll x,y;
+        extgcd(k,n,x,y);
+        ll ans = -s * x;
+        ans %= n;
+        if(ans < 0) ans += n;
+        cout << ans << endl;
+    }
 }
 
 int main(){
-    ll X,Y;
-    cin >> X >> Y;
-    rep(i,2){
-        ll x,y;
-        cin >> x >> y;
-        pll ans = CRT(X,Y,x,y);
-        if(ans.second == -1){
-            cout << -1 << endl;
-            return 0;
-        }
-        X = ans.first;
-        Y = ans.second;
-        //cout <<  X  << " " << Y << endl;
-    }
-    if(X ==0){
-        cout << Y << endl;
-    }else{
-        cout << X << endl;
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
     }
     return 0;
 }
-
-/*
-x % m1 == b1 && x % m2 == b2 
-<=>  x % lcm(m1,m2)( == m) == r
-を満たすを求める。
-rはつまり上の二式をみたす最小の数
-中国剰余定理　Chinese Remaninder Theorem
-*/

9f116b80518634931f5058ea26b1849c2be68994
kappybar <newton7233@outlook.jp>
2021-04-15 22:44:19 +0900

    Unionfind

--- Data_Structure/Unionfind.cpp
+++ Data_Structure/Unionfind.cpp
@@ -7,18 +7,18 @@ const int MOD = 1000000007;
 #define rep(i,n) for(int i=0;i<n;i++)
 
 struct Unionfind{
-    vector<int> parents;
     int n;
+    vector<int> parents;
     Unionfind(int n):n(n),parents(n,-1){ }
 
     int find(int x){
         if(parents[x] < 0) return x;
-        else return parents[x] = find(parents[x]);
+        return parents[x] = find(parents[x]);
     }
 
     void unite(int x,int y){
         x = find(x);
-        y =  find(y);
+        y = find(y);
 
         if(x==y) return;
         if(parents[x] > parents[y]) swap(x,y);
@@ -26,32 +26,32 @@ struct Unionfind{
         parents[y] = x;
     }  
 
-   int size(int x){
-       return -parents[find(x)];
-   }
+    int size(int x){
+        return -parents[find(x)];
+    }
 
-   bool same(int x,int y){
-       return find(x)==find(y);
-   }
+    bool same(int x,int y){
+        return find(x)==find(y);
+    }
 
-   vector<int> members(int x){
-       int root = find(x);
-       vector<int> member;
-       for(int i=0;i<n;i++){
-           if(find(i)==root ){
-               member.push_back(i);
-           }
-       }
-      return member;
-  }
+    vector<int> members(int x){
+        int root = find(x);
+        vector<int> member;
+        for(int i=0;i<n;i++){
+            if(find(i)==root ){
+                member.push_back(i);
+            }
+        }
+        return member;
+    }
 
-  int group_cnt(){
-      int c = 0;
-      rep(i,n){
-          if(parents[i] < 0) ++c;
-      }
-      return c;
-  }
+    int group_cnt(){
+        int c = 0;
+        for(int i=0;i<n;i++){
+            if(parents[i] < 0) ++c;
+        }
+        return c;
+    }
 };
 
 int main(){

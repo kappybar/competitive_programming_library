f8e5388cb84dc922e37d64baee0586d8a8ec51fb
kappybar <newton7233@outlook.jp>
2021-08-17 17:44:17 +0900

    sieve

--- Math/Sieve.cpp
+++ Math/Sieve.cpp
@@ -1,93 +1,106 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<n;i++)
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define chmin(x,y) x = min((x),(y))
+#define chmax(x,y) x = max((x),(y))
+#define popcount(x) __builtin_popcount(x)
 using namespace std;
-typedef long long ll;
-typedef pair<int,int> P;
+using ll = long long ;
+using P = pair<int,int> ;
+using pll = pair<long long,long long>;
 const int INF = 1e9;
+const long long LINF = 1e17;
 const int MOD = 1000000007;
-
+//const int MOD = 998244353;
+const double PI = 3.14159265358979323846;
 
 struct Sieve {
-        int n;
-        vector<int> f,primes;
-        
-        Sieve(int n=1):n(n),f(n+1){
-            f[0] = f[1] = -1;
-            for(long long i=2;i<=n;++i){
-                if(f[i]) continue;
-                primes.push_back(i);
-                f[i] = i;
-                for(long long j = i*i;j <= n;j += i) {
-                    if(!f[j]) f[j] = i;
-                }
+    int n;
+    vector<int> f,primes,mobius;
+    
+    Sieve(int n=1):n(n),f(n+1),mobius(n+1){
+        f[0] = f[1] = -1;
+        mobius[1] = 1;
+        primes.reserve(n);
+        for(long long i=2;i<=n;++i){
+            if(f[i]) continue;
+            primes.push_back(i);
+            f[i] = i;
+            mobius[i] = -1;
+            for(long long j = 2*i;j <= n;j += i) {
+                if(!f[j]) f[j] = i;
+                if((j/i)%i == 0) mobius[j] = 0;
+                else mobius[j] = -mobius[j];
             }
         }
-        
-        bool isPrime(int x){return f[x] == x;}
-        
-        vector<int> factorList(int x){
-            vector<int> res;
-            while(x != 1){
-                res.push_back(f[x]);
+    }
+    
+    bool isPrime(int x){return f[x] == x;}
+    
+    //高速素因数分解
+    vector<pair<int,int>> factor(int x){
+        vector<pair<int,int>> res;
+        while(x > 1){
+            int p = f[x];
+            int c = 0;
+            while(f[x] == p){
+                ++c;
                 x /= f[x];
             }
-            return res;
+            res.emplace_back(p,c);
         }
-        
-        vector<pair<int,int>> factor(int x){
-            vector<int> fl = factorList(x);
-            if(fl.size() == 0) return {};
-            vector<pair<int,int>> res(1,pair<int,int>(fl[0],0));
-            for(int p:fl){
-                if(res.back().first == p){
-                    res.back().second ++;
-                }else{
-                    res.emplace_back(p,1);
-                }
-            }
-            return res;
-        }
+        return res;
+    }
 
-        long long divisorCalc(long long x){
-            long long res = 1;
-            long long now = -1;
-            long long nowCnt = 0;
-            while(x != 1){
-                if(now==f[x]) ++ nowCnt;
-                else{
-                    res *= nowCnt + 1;
-                    now = f[x];
-                    nowCnt = 1;
+    //高速約数列挙
+    vector<int> divisor(int x){
+        vector<int> res(1,1);
+        auto pf = factor(x);
+        for(auto [p,c]:pf) {
+            int sz = (int)res.size();
+            for(int i=0;i<sz;i++){
+                int v = 1;
+                for(int j=0;j<c;j++){
+                    v *= p;
+                    res.push_back(res[i] * v);
                 }
-                x /= f[x];
             }
-            res *= nowCnt+1;
-            return res;
         }
-        
+        return res;
+    }
+
+    //約数の個数
+    long long divisor_count(int x){
+        auto pf = factor(x);
+        long long res = 1;
+        for(auto [p,c]:pf) res *= c+1;
+        return res;
+    }
 };
 
-/*エラトステネスの篩　ついでに高速素因数分解もできる。　fはi以下の最小の素数を表していて、primes は素数を列挙している
-isPrime で素数判定　factorListで素因数分解した結果の素数の集合がわかる。　factorでは素因数分解した結果の数がランググレンす圧縮された形でreturn 
-される ex) factorList(12) = {2,2,3} factor(12) = {(2,2),(3,1)}。
-divisorCalcで約数の個数を求める*/
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i,n) cin >> a[i];
+    Sieve sieve(1000005);
 
-int main() {
-        Sieve sieve(1000005);
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        rep(i,n) cin >> a.at(i);
-        map<int,int> mp;
-        rep(i,n){
-                 auto f = sieve.factor(a[i]);
-                for(auto p:f){
-                        mp[p.first] = max(mp[p.first],p.second);
-                }
+    vector<int> cnt(1000005,0);
+    bool pairwise = true;
+    rep(i,n){
+        auto pf = sieve.factor(a[i]);
+        for(auto [a,b]:pf){
+            cnt[a] ++;
+            if(cnt[a] > 1) pairwise = false;
         }
-        
+    }
+
+    if(pairwise){
+        cout << "pairwise coprime" << endl;
+    }else{
+        int g = 0;
+        rep(i,n) g = gcd(g,a[i]);
+        if(g==1) cout << "setwise coprime" << endl;
+        else cout << "not coprime" << endl;
+    }
+    return 0;
 }
-/*このように使う*/
-/*https://atcoder.jp/contests/abc152/submissions/9686995*/
-                                
-                        

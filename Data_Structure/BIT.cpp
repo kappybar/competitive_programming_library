9b15f5fed37086c5648d3a7d27b96fb142d98f10
kappybar <newton7233@outlook.jp>
2022-03-28 22:09:30 +0900

    refactor BIT

--- Data_Structure/BIT.cpp
+++ Data_Structure/BIT.cpp
@@ -1,70 +1,71 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> pp;
-const int INF = 1e9;
-const int MOD = 1000000007;
-#define rep(i,n) for(int i=0;i<n;i++)
 
-
-//1-indexedに注意！！！
+template <typename T>
 struct BIT{
-    private:
-    int n,n_;
-    vector<long long> bit;
+private:
+    int n, len;
+    std::vector<T> bit;
 
-    public:
-    BIT(int n):n(n){
-        bit.resize(n+1);
-        n_ = 1;
-        while(n_<=n) n_<<=1;
-        n_ >>= 1;
+public:
+    BIT(int n):n(n) {
+        bit.assign(n+1, 0);
+        len = 1;
+        while ((len << 1) <= n) len <<= 1;
     }
-    //1-index
-    long long sum(int i){
-        long long res = 0;
-        while(i > 0){
+
+private:
+    // 1-index
+    // sum of [1, i]
+    T prefix_sum(int i) {
+        T res = 0;
+        while(i > 0) {
             res += bit[i];
-            i -= i&-i;
+            i -= i & -i;
         }
         return res;
     }
-    //1-index
-    void add(int i,long long val){
-        while(i <= n){
-            bit[i] += val;
-            i += i&-i;
-        }
+
+public:
+    // 0-index
+    // sum of [l, r)
+    T sum(int l, int r) {
+        // [0, r) - [0, l)
+        return prefix_sum(r) - prefix_sum(l);
     }
-    //return 1-index
-    int lowerbound(long long w){
-        if(w <= 0) return 0;
+
+
+    // 0-index
+    // add val to a[i]
+    void add(int i, T val) {
+        i++;
+        while(i <= n) {
+            bit[i] += val;
+            i += i & -i;
+        }
+        return;
+    }
+
+    // 0-index
+    // constraints : \forall i a[i] >= 0
+    // The smallest x that satisfies [a[0] + a[1] + ... + a[x] >= w]
+    int lowerbound(T w) {
+        if (w <= 0) return 0;
         int x = 0;
-        for(int k=n_;k>0;k>>=1){
-            if(x+k<=n&&bit[x+k]<w){
-                w -= bit[x+k];
+        for (int k = len;k > 0;k >>= 1) {
+            if(x + k <= n && bit[x + k] < w){
+                w -= bit[x + k];
                 x += k;
             }
         }
-        return x+1;
+        return x;
+    }
+
+    void debug(void) {
+        std::cout << "sum[0:i] :"  << std::endl;
+        for (int i = 0;i <= n; i++) {
+            std::cout << sum(0, i) << " "; 
+        }
+        std::cout << std::endl;
+        return;
     }
 };
-
-
-
-int main(){
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        rep(i,n) cin >> a.at(i);
-        BIT bit(n);
-        ll ans = 0;
-        rep(i,n){
-                ans += i-bit.sum(a.at(i));
-                bit.add(a.at(i),1);
-        }
-        cout << ans << endl;
-}
-                        
-                        
-        

65aa9c063b2c8ad6fd4fdf737e49395f73e54dae
kappybar <newton7233@outlook.jp>
2022-03-28 23:06:34 +0900

    refactor Unionfind

--- DataStructure/Unionfind.cpp
+++ DataStructure/Unionfind.cpp
@@ -1,71 +1,53 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> pp;
-const int INF = 1e9;
-const int MOD = 1000000007;
-#define rep(i,n) for(int i=0;i<n;i++)
 
 struct Unionfind{
+private:
     int n;
-    vector<int> parents;
-    Unionfind(int n):n(n),parents(n,-1){ }
+    std::vector<int> parents;
 
-    int find(int x){
+public:
+    Unionfind(int n):n(n), parents(n,-1) {}
+
+    int find(int x) {
         if(parents[x] < 0) return x;
         return parents[x] = find(parents[x]);
     }
 
-    void unite(int x,int y){
+    void unite(int x,int y) {
         x = find(x);
         y = find(y);
 
-        if(x==y) return;
-        if(parents[x] > parents[y]) swap(x,y);
+        if(x == y) return;
+        if(parents[x] > parents[y]) std::swap(x, y);
         parents[x] += parents[y];
         parents[y] = x;
+        return;
     }  
 
-    int size(int x){
+    int size(int x) {
         return -parents[find(x)];
     }
 
-    bool same(int x,int y){
-        return find(x)==find(y);
+    bool same(int x, int y) {
+        return find(x) == find(y);
     }
 
-    vector<int> members(int x){
+    std::vector<int> members(int x) {
         int root = find(x);
-        vector<int> member;
-        for(int i=0;i<n;i++){
-            if(find(i)==root ){
+        std::vector<int> member;
+        for(int i = 0;i < n; i++){
+            if(find(i) == root ){
                 member.push_back(i);
             }
         }
         return member;
     }
 
-    int group_cnt(){
+    int group_cnt(void) {
         int c = 0;
-        for(int i=0;i<n;i++){
+        for(int i = 0;i < n; i++){
             if(parents[i] < 0) ++c;
         }
         return c;
     }
 };
-
-int main(){
-    int n,q;
-    cin >> n >> q;
-    Unionfind uf(n);
-    rep(i,q){
-        int c,x,y;
-        cin >> c >> x >> y;
-        if(c==0) uf.unite(x,y);
-        else{
-            if(uf.same(x,y)) cout << 1 << endl;
-            else cout << 0 << endl;
-        }
-    }
-    return 0;
-}

e18cffbd7afd038abb88155e89ab6a9196a26396
kappybar <newton7233@outlook.jp>
2022-03-29 23:48:02 +0900

    refactor SegmentTree

--- DataStructure/SegmentTree.cpp
+++ DataStructure/SegmentTree.cpp
@@ -1,70 +1,73 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-using namespace std;
-using ll = long long ;
-using P = pair<int,int> ;
-using pll = pair<long long,long long>;
-constexpr int INF = 1e9;
-constexpr long long LINF = 1e17;
-constexpr int MOD = 1000000007;
-constexpr double PI = 3.14159265358979323846;
 
-//抽象化したセグメントツリー 
-
-struct Segmenttree{
-    using F = function<long long(long long,long long)>;
-    F f;
-    const long long unit;
+template <typename S, S (*op)(S, S), S (*e)()>
+struct SegmentTree {
+private:
     int n;
-    vector<long long> node;
+    std::vector<S> node;
 
-    Segmenttree(F f,long long unit,vector<long long> v):f(f),unit(unit){
+public:
+    SegmentTree(int n):SegmentTree(std::vector<S>(n, e()))  {}
+    SegmentTree(const std::vector<S> &v) {
         n = (int)(v.size());
         node.resize(2*n);
-        for(int i=0;i<n;i++) node[i+n] = v[i];
-        for(int i=n-1;i>=0;i--) node[i] = f(node[i<<1],node[i<<1|1]);
+        for(int i = 0;i < n; i++) node[i+n] = v[i];
+        for(int i = n - 1;i >= 0; i--) node[i] = op(node[i << 1], node[i << 1 | 1]);
     }
-    //i番目の要素をxに変更する
-    void update(int i,long long x){
+
+    // a[i] = x
+    void set(int i, S x) {
         i += n;
         node[i] = x;
-        while(i>1){
+        while(i > 1) {
             i >>= 1;
-            node[i] = f(node[i<<1],node[i<<1|1]);
+            node[i] = op(node[i << 1], node[i << 1 | 1]);
         }
+        return;
     }
-    //[l,r)の要素
-    long long query(int l,int r){
-        long long vl = unit,vr = unit;
+
+    // a[i]
+    S get(int i) {
+        return node[i + n];
+    }
+
+    // op(a[l], a[l+1], ... ,a[r-1])
+    S query(int l, int r) {
+        S vl = e(), vr = e();
         l += n; r += n;
-        while(l<r){
-            if(l&1) vl = f(vl,node[l++]);
-            if(r&1) vr = f(vr,node[--r]);
+        while(l < r) {
+            if(l & 1) vl = op(vl, node[l++]);
+            if(r & 1) vr = op(vr, node[--r]);
             l >>= 1; r >>= 1;
         }
-        return f(vl,vr);
+        return op(vl, vr);
     }
-    //i番目の要素にアクセス
-    long long getval(int i){
-        return node[i+n];
-    }
+
 };
 
-int main(){
-    int n,q;
-    cin >> n >> q;
-    auto f = [](ll a,ll b){ return min(a,b);}; //関数
-    ll unit = INT_MAX; //単位元
-    Segmenttree seg(f,unit,vector<ll>(n,unit));
+// // min
+// long long e() {
+//     return (1LL << 31) - 1;
+// }
 
-    while(q--){
-        int t,x,y;
-        cin >> t >> x >> y;
-        if(t==0){
-            seg.update(x,y);
-        }else{
-            cout << seg.query(x,y+1) << endl;
-        }
-    }
-    return 0;
-}
+// long long op(long long a, long long b) {
+//     return std::min(a, b);
+// }
+
+// // sum
+// long long e() {
+//     return 0LL;
+// }
+
+// long long op(long long a, long long b) {
+//     return a + b;
+// }
+
+// // max
+// long long e() {
+//     return 0LL;
+// }
+
+// long long op(long long a, long long b) {
+//     return std::max(a, b);
+// }

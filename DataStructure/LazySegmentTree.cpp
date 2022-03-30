bec27e0f70e22d2654c89c99d834816ffc38f43c
kappybar <newton7233@outlook.jp>
2022-03-30 21:20:57 +0900

    refactor LazySegmentTree

--- DataStructure/LazySegmentTree.cpp
+++ DataStructure/LazySegmentTree.cpp
@@ -1,158 +1,201 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-using namespace std;
-using ll = long long ;
-using P = pair<int,int> ;
-using pll = pair<long long,long long>;
-constexpr int INF = 1e9;
-constexpr long long LINF = 1e17;
-constexpr int MOD = 1000000007;
-constexpr double PI = 3.14159265358979323846;
 
-/*
-遅延評価付きセグメント木
-f Mono * Mono -> Mono node配列のMonoを二つ作用させる関数。
-g Mono * Ope -> Mono nodeにlazy配列を作用させる関数。
-h Ope * Ope -> Ope　lazy配列の作用を結合させる関数。
-tu Monoの単位元
-eu Opeの単位元
-f と hは掛け算のようなことができて、gは指数のようなことできればいい。
-*/
+template<typename S, S (*op)(S, S), S (*e)(), typename F, S (*mapping)(F, S), F (*composition)(F, F), F (*id)()>
+struct LazySegmentTree {
+private:
+    int n, height;
+    std::vector<S> node;
+    std::vector<F> lazy;
 
-template<typename Mono,typename Ope>
-struct LazySegmentTree{
-    using F = function<Mono(Mono,Mono)>;
-    using G = function<Mono(Mono,Ope)>;
-    using H = function<Ope(Ope,Ope)>;
-    int n,height;
-    F f; G g; H h; Mono tu; Ope eu;
-    vector<Mono> node;
-    vector<Ope> lazy;
-
-    LazySegmentTree(F f,G g,H h,Mono tu,Ope eu):f(f),g(g),h(h),tu(tu),eu(eu){}
-
-    void init(vector<Mono> v){
-        int n_ = (int)(v.size());
-        n = 1;height = 0;
-        while(n < n_) n <<= 1,++ height;
-        node.assign(2*n,tu);
-        lazy.assign(2*n,eu);
-        for(int i=0;i<n_;i++) node[i+n] = v[i];
-        for(int i=n-1;i>0;i--) node[i] = f(node[i<<1],node[i<<1|1]);
+public:
+    LazySegmentTree(int n): LazySegmentTree(std::vector<S>(n, e())) {};
+    LazySegmentTree(const std::vector<S> &v) {
+        int vs = (int)v.size();
+        n = 1;
+        height = 0;
+        while (n < vs) n <<= 1, height++;
+        S es = e();
+        node.assign(2*n, es);
+        F ef = id();
+        lazy.assign(2*n, ef);
+        for (int i = 0;i < vs; i++) node[i + n] = v[i];
+        for (int i = n - 1;i >= 0; i--) node[i] = op(node[i << 1], node[i << 1 | 1]);
+        return;
     }
 
-    Mono eval(int k){
-        return (lazy[k]==eu)?node[k]:g(node[k],lazy[k]);
+private:  
+    S eval(int i) {
+        return lazy[i] == id() ? node[i] : mapping(lazy[i], node[i]);
     }
 
-    void prop(int k){
-        if(lazy[k]==eu) return;
-        lazy[k<<1] = h(lazy[k<<1],lazy[k]);
-        lazy[k<<1|1] = h(lazy[k<<1|1],lazy[k]);
-        node[k] = eval(k);
-        lazy[k] = eu;
+    void propagate(int i) {
+        if(lazy[i] == id()) return;
+        lazy[i << 1] = composition(lazy[i << 1], lazy[i]);
+        lazy[i << 1 | 1] = composition(lazy[i << 1 | 1], lazy[i]);
+        node[i] = eval(i);
+        lazy[i] = id();
+        return;
     }
 
-    void prop_above(int k){
-        for(int i=height;i>0;i--) prop(k>>i);
+    void propagate_above(int i) {
+        for(int j = height;j > 0; j--) propagate(i >> j);
+        return;
     }
 
-    void recalc_above(int k){
-        while(k>0){
-            k >>= 1;
-            node[k] = f(eval(k<<1),eval(k<<1|1));
+    void recalc_above(int i) {
+        while(i > 0) {
+            i >>= 1;
+            node[i] = op(eval(i << 1), eval(i << 1 | 1));
         }
+        return;
     }
 
-    void set_val(int k,Mono x){
-        k += n;
-        prop_above(k);
-        node[k] = x;
-        lazy[k] = eu;
-        recalc_above(k);
+public:
+    // a[i] = x
+    void set(int i, S x) {
+        i += n;
+        propagate_above(i);
+        node[i] = x;
+        lazy[i] = id();
+        recalc_above(i);
+        return;
     }
 
-    void update(int l,int r,Ope x){
+    // i \in [l, r) a[i] = mapping(x, a[i])
+    void apply(int l, int r, F x) {
         if(l >= r) return;
-        l += n;r += n-1;
-        prop_above(l);
-        prop_above(r);
-        int a = l,b = r;
-        ++ r;
-        while(l < r){
-            if(l&1) lazy[l] = h(lazy[l],x), ++l;
-            if(r&1) --r, lazy[r] = h(lazy[r],x);
+        l += n;r += n - 1;
+        propagate_above(l);
+        propagate_above(r);
+        int a = l, b = r;
+        ++r;
+        while(l < r) {
+            if(l & 1) lazy[l] = composition(lazy[l], x), ++l;
+            if(r & 1) --r, lazy[r] = composition(lazy[r], x);
             l >>= 1;r >>= 1;
         }
         recalc_above(a);
         recalc_above(b);
+        return;
     }
 
-    Mono query(int l,int r){
-        if(l >= r) return tu;
+    // op(a[l], a[l+1], ... a[r-1])
+    S prod(int l, int r) {
+        if(l >= r) return e();
         l += n;r += n-1;
-        prop_above(l);
-        prop_above(r);++r;
-        Mono vl = tu,vr = tu;
-        while(l < r){
-            if(l&1) vl = f(vl,eval(l)), ++l;
-            if(r&1) --r, vr = f(eval(r),vr);
+        propagate_above(l);
+        propagate_above(r);
+        ++r;
+        S vl = e(), vr = e();
+        while(l < r) {
+            if(l & 1) vl = op(vl, eval(l)), ++l;
+            if(r & 1) --r, vr = op(eval(r), vr);
             l >>= 1;r >>= 1;
         }
-        return f(vl,vr);
+        return op(vl, vr);
     }
 };
 
-struct mono{
-    ll two,one,zero;
-};
+// // RMQ, RUQ
+// long long op(long long a, long long b) {
+//     return std::min(a, b);
+// }
 
-int main(){
-    auto f = [](mono l,mono r){
-        l.two += r.two;
-        l.one += r.one;
-        l.zero += r.zero;
-        return l;
-    };
-    auto g = [](mono l,ll x){
-        l.two =  l.two + 2 * x * l.one + x * x * l.zero;
-        l.one = l.one + x * l.zero;
-        l.zero = l.zero;
-        return l;
-    };
-    auto h = [](ll a,ll b){
-        return a+b;
-    };
-    LazySegmentTree<mono,ll> seg(f,g,h,mono{0,0,0},0);
+// long long e() {
+//     return (1LL << 31) - 1;
+// }
 
-    int n;
-    cin >> n;
-    vector<mono> a(n);
-    rep(i,n){
-        ll b;cin >> b;
-        a[i] = mono{b*b,b,1};
-    }
-    seg.init(a);
+// long long mapping(long long a, long long b) {
+//     return a;
+// }
 
-    int q;
-    cin >> q;
-    while(q--){
-        int t;
-        cin >> t;
-        if(t==1){
-            int l,r;
-            ll x;
-            cin >> l >> r >> x;
-            --r;--l;
-            seg.update(l,r+1,x);
-        }else{
-            int l,r;
-            cin >> l >> r;
-            --l;--r;
-            cout << seg.query(l,r+1).two << endl;
-        }
-    }
+// long long composition(long long a, long long b) {
+//     return b;
+// }
+
+// long long id() {
+//     return (1LL << 31) - 1;
+// }
 
 
-    return 0;
-}
+// // RMQ, RAQ
+// long long op(long long a, long long b) {
+//     return std::min(a, b);
+// }
+
+// long long e() {
+//     return INF;
+// }
+
+// long long mapping(long long a, long long b) {
+//     return b + a;
+// }
+
+// long long composition(long long a, long long b) {
+//     return a + b;
+// }
+
+// long long id() {
+//     return 0LL;
+// }
+
+
+// // RSQ, RAQ
+// struct S {
+//     long long sum, sz;
+// };
+
+// S op(S a, S b) {
+//     return S{a.sum + b.sum, a.sz + b.sz};
+// }
+
+// S e() {
+//     return S{0, 0};
+// }
+
+// S mapping(long long a, S b) {
+//     return S{b.sum + a * b.sz, b.sz};
+// }
+
+// long long composition(long long a, long long b) {
+//     return a + b;
+// }
+
+// long long id() {
+//     return 0LL;
+// }
+
+// // RSQ, RUQ
+// struct S {
+//     long long sum, sz;
+// };
+
+// struct F {
+//     long long x;
+//     bool change;
+
+//     bool operator == (const F& r) {
+//         return x == r.x && change == r.change;
+//     }
+// };
+
+// S op(S a, S b) {
+//     return S{a.sum + b.sum, a.sz + b.sz};
+// }
+
+// S e() {
+//     return S{0, 0};
+// }
+
+// S mapping(F a, S b) { 
+//     return a.change ? S{a.x * b.sz, b.sz} : b;
+// }
+
+// F composition(F a, F b) {
+//     return b;
+// }
+
+// F id() {
+//     return F{0LL, false};
+// }
+
